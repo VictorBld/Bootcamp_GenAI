b8481d1486367f3ce54905acf5bf388a42ccba19
Victor Blondeau <blondeau.victor@gmail.com>
2025-02-10 11:11:41 +0100

    nouveau code

---
+++ W1/D1/W1D1Cours.py
@@ -0,0 +1,71 @@
+# name = "Alice"
+# age = 30
+# city = "New York"
+# tasks= None
+
+# print(f"Hello, {name}! You are {age} years old and live in {city}.")
+
+
+
+# age2=int(input("Entrez votre age:"))
+# x=100-age2
+# print(f"You will turn 100 in {x} years")
+
+# nom=input("What's your name ?")
+# if len(nom)<5:
+#     print("You have a short name :)")
+
+
+# nombre=int(input("Put a number between 1 & 100: "))
+# if nombre%5==0 and nombre%3==0:
+#     print("FizzBuzz")
+# elif nombre%3==0:
+#     print("Fizz")
+# elif nombre%5==0:
+#     print("Buzz")
+
+
+#Conversion miles kilometres
+
+# m=float(input("give a numbre of miles :"))
+
+# new_m=m*1.60934
+
+# print(f"{m} miles is equal to {new_m} km!")
+
+
+# name = 'John Doe'
+# if len(name)>20:
+#     print(f"Name{name} is more than 20 chars long")
+#     length_description = 'long'
+# elif len(name)>15:
+#     print(f"Name {name} is more than 15 chars long")
+#     length_description = 'semi long'
+# elif len(name)>10:
+#     print(f"Name {name} is more than 10 chars long")
+#     length_description = 'semi long'
+# elif len(name) in {8,9,10,20}:
+#     print(f"Name {name} is 8, 9 or 10 20 chars long")
+#     length_description = 'semi short'
+# else:
+#     print(f"Name {name} is a short name")
+#     length_description = 'short'
+
+
+n1=input("entrez un numéro:")
+n2=input("entrez un numéro:")
+n3=input("entrez un numéro:")
+l=[n1,n2,n3]
+print(f"le plus grand est {max(l)}!")
+
+
+# guest=int(input("How many people will attend the wedding ?"))
+
+# if guest in range(0,50):
+#     print("The price is 4 000$")
+# elif guest in range (51,100):
+#     print("The price is 10 000$")
+# elif guest in range (101,200):
+#     print("The price is 15 000$")
+# else:
+#     print("The price is 20 000")
